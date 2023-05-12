26322f116ef1133b4da8cbb0b3199382c321530a
Vitor Pereira <vitorpereiradeoli@gmail.com>
2023-05-12 15:50:33 -0300

    :sparkles: feat: observer to listen to permission change

--- lib/src/modules/onboard/pages/onboard_page.dart
+++ lib/src/modules/onboard/pages/onboard_page.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:me_adota/src/modules/onboard/controllers/onboard_controller.dart';
 import 'package:me_adota/src/modules/onboard/widgets/next_page_button.dart';
 import 'package:me_adota/src/modules/onboard/widgets/page_indicator.dart';
 import 'package:me_adota/src/modules/onboard/widgets/page_slider.dart';
 import 'package:me_adota/src/modules/onboard/widgets/skip_button.dart';
+import 'package:me_adota/src/shared/controllers/localization_controller.dart';
 import 'package:me_adota/src/shared/styles/theme.dart';
-import 'package:me_adota/src/shared/widgets/app_bottom_sheet.dart';
 import 'package:provider/provider.dart';
 
-/**
- * TODO: Refactor this page to be a StatefulWidget, using WidgetBinding to
- * listen to the lifecycle events
- */
 class OnboardPage extends StatefulWidget {
   const OnboardPage({super.key});
 
@@ -19,7 +14,27 @@ class OnboardPage extends StatefulWidget {
   State<OnboardPage> createState() => _OnboardPageState();
 }
 
-class _OnboardPageState extends State<OnboardPage> {
+class _OnboardPageState extends State<OnboardPage> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      final controller = context.read<LocalizationController>();
+      controller.checkInitialPermission();
+    }
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
